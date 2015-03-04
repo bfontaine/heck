20ffe9dd25bbe0fe1aa9d6c4a604cb36bdb91368
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-04 16:13:16 +0100

    parsing tests

--- repl_test.go
+++ repl_test.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"github.com/franela/goblin"
 	o "github.com/onsi/gomega"
 	"testing"
 )
+
+func checkParsing(g *goblin.G, line string) {
+	repl := NewRepl()
+	repl.line = line
+	// we don't use o.BeNil here because the parsing errors take too much place
+	// when they're pretty-printed
+	o.Expect(repl.Eval() == nil).To(o.BeTrue())
+}
 
 func TestRepl(t *testing.T) {
 	g := goblin.Goblin(t)
@@ -37,6 +46,23 @@ func TestRepl(t *testing.T) {
 				o.Expect(repl.Eval()).To(o.BeNil())
 				o.Expect(repl.lastResult).To(o.Equal(int64(-3268)))
 			})
+
+			// these were generated using:
+			// $ abnfgen -y 10 heck.abnf
+
+			lines := []string{
+				"   d%          (   a   )   /       a",
+				"   (   \tA-  A+  A   )   /   -   (   A   )",
+				"\t\t\tdaA",
+				"~          2aa-    ~\tA-  -   (   A   )   +   A",
+			}
+
+			for i, line := range lines {
+				desc := fmt.Sprintf("Should parse valid expressions (%d)", i+1)
+				g.It(desc, func() {
+					checkParsing(g, line)
+				})
+			}
 		})
 	})
 }
